d70cda12681924d0d8c4345922220d04d48b0d82
Ricardo <rica14062004@gmail.com>
2023-11-04 11:03:59 -0300

    Creacion JPanel PanelPrincipal

---
+++ Tarea-3/src/grafica/PanelPrincipal.java
@@ -0,0 +1,18 @@
+package grafica;
+import javax.swing.*;
+import java.awt.*;
+
+public class PanelPrincipal extends JPanel{
+    public PanelPrincipal(){
+        this.setLayout(new GridLayout(1,2));
+        this.setBackground(Color.WHITE);
+    }
+    @Override
+    public void paint(Graphics g){
+        super.paint(g);
+
+
+    }
+
+}
+
