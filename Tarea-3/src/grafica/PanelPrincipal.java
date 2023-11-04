c08a974a99acc253b5ca378e2bc0b2e767146d40
Ricardo <rica14062004@gmail.com>
2023-11-04 13:49:21 -0300

    Configuracion inicial ventana

--- Tarea-3/src/grafica/PanelPrincipal.java
+++ Tarea-3/src/grafica/PanelPrincipal.java
@@ -3,9 +3,15 @@ import javax.swing.*;
 import java.awt.*;
 
 public class PanelPrincipal extends JPanel{
+    private PanelComprador com;
+    private PanelExpendedor exp;
     public PanelPrincipal(){
+        exp = new PanelExpendedor();
+        com = new PanelComprador();
         this.setLayout(new GridLayout(1,2));
         this.setBackground(Color.WHITE);
+        this.add(exp);
+        this.add(com);
     }
     @Override
     public void paint(Graphics g){
--- Tarea-3/src/grafica/Ventana.java
+++ Tarea-3/src/grafica/Ventana.java
@@ -2,7 +2,11 @@ package grafica;
 import javax.swing.*;
 
 public class Ventana extends JFrame{
+    PanelPrincipal panel;
     public Ventana(){
+        panel = new PanelPrincipal();
+        this.add(panel);
+
         this.setVisible(true);
         this.setSize(1400, 800);
         this.setLocationRelativeTo(null);
