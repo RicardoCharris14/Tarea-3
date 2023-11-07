c3590c6d118a184a63d29be2ece5610efe279e59
VicenteRamirez15 <vramirezt15@gmail.com>
2023-11-07 15:36:19 -0300

    Creación de clase PanelDepositoSnickers y modificacion de imagen de snickers

--- Tarea-3/src/grafica/PanelDepositoSnickers.java
+++ Tarea-3/src/grafica/PanelDepositoSnickers.java
@@ -4,9 +4,33 @@ import javax.swing.*;
 import java.awt.*;
 
 public class PanelDepositoSnickers extends JPanel {
+    private Image imSnickers;
+    private int cantidadProducto;
     public PanelDepositoSnickers(){
+        cantidadProducto = 5;
+        imSnickers = new ImageIcon("src/grafica/imagenes/snickersparado.png").getImage();
 
-        this.setLayout(new GridLayout(1,5));
         this.setBackground(Color.BLACK);
+    }
+    @Override
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+        int anchoPanel = this.getWidth();
+        int altoPanel = this.getHeight();
+        //rectangulo fondo deposito
+        g.setColor(Color.GRAY);
+        g.fillRect(anchoPanel*4/100,altoPanel*14/100,anchoPanel*94/100,altoPanel*79/100);
+        g.drawLine(0,0,anchoPanel*4/100,altoPanel*14/100);
+        g.drawLine(0,altoPanel,anchoPanel*4/100,altoPanel*92/100);
+        g.drawLine(anchoPanel,0,anchoPanel*98/100,altoPanel*15/100);
+        g.drawLine(anchoPanel,altoPanel,anchoPanel*98/100,altoPanel*92/100);
+        //productos en el deposito
+        int tamanoIm = anchoPanel/5;
+        for(int i = 0 ; i < cantidadProducto ; i++){
+            g.drawImage(imSnickers,tamanoIm*i,altoPanel/4,tamanoIm,tamanoIm,this);
+        }
+
+
     }
 }
