e32fe768f993fb07ac04bf5b3012a2ae13a19c48
VicenteRamirez15 <vramirezt15@gmail.com>
2023-11-07 20:24:50 -0300

    Creación de clase PanelMonade1000

--- Tarea-3/src/grafica/PanelMoneda1000.java
+++ Tarea-3/src/grafica/PanelMoneda1000.java
@@ -1,9 +1,24 @@
 package grafica;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class PanelMoneda1000 extends JPanel {
+
+    private Image imMoneda1000;
+
     public PanelMoneda1000(){
 
+        imMoneda1000 = new ImageIcon("src/grafica/imagenes/moneda1000.png").getImage();
+
+        this.setLayout(null);
+        this.setBackground(Color.white);
+    }
+    @Override
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        g.drawImage(imMoneda1000,0,0,this.getWidth(),this.getHeight(),this);
     }
 }
+
+
